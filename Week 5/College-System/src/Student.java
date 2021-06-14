2d85ab439f5ceb39c226641f1cf26ac99312ddaa
heskethg <85505664+heskethg@users.noreply.github.com>
2021-06-14 14:13:31 -0400

    Updated readMarks and calculateGpa

--- Week 5/College-System/src/Student.java
+++ Week 5/College-System/src/Student.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Student extends Person {
     
     protected int studentNumber;
@@ -19,22 +21,43 @@ public class Student extends Person {
 
     /* reads name, std num, phon, email, program */
     public void readInfo() {
-
+        System.out.println("Enter program name: ");
+        System.out.println("Enter student number: ");
+        System.out.println("Enter first name: ");
+        System.out.println("Enter last name: ");
+        System.out.println("Enter email ID: ");
+        System.out.println("Enter phone number: ");  
     }
+
     /* reads number of courses
     loops for number of courses to read each mark 
     calls calculateGpa() */
     public void readMarks() {
-
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number of courses: ");
+        int k = sc.nextInt();
+        double[] marks = new double[k];
+        for (int i = 1; i <= k; i++) {
+            System.out.println("Enter mark " + i + ": ");
+            marks[i - 1] = sc.nextDouble();
+        }
+        calculateGpa(marks);
     }
 
     /* method to calculate the gpa of a student 
-    assigns calculated mark to this.gpa*/
+    assigns calculated mark to this.gpa 
+    convert from percentage to 4.0 scale using x/20 - 1 
+    average all results for final gpa */
     private void calculateGpa(double[] marks) {
-        
+        double grd = 0.0;
+        for (int i = 0; i < marks.length; i++) {
+            grd += (marks[i]/20) - 1;
+        }
+        this.gpa = grd/marks.length;
     }
 
-    /* Specifc method to print name, std num, phone, email, program */
+    /* Specifc method to print name, std num, phone, email, program 
+    formatted with printf */
     public void printInfo() {
 
     }
